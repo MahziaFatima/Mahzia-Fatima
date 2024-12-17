b5068cd777c90ba7a11050c43b83b4b735b40670
Maha Fatima <fatimamahzia@gmail.com>
2024-12-17 14:44:35 +0500

    Add files via upload

---
+++ indexscript.js
@@ -0,0 +1,34 @@
+
+let name ="maha"
+console.log("Hello, My name is ",name) ;
+
+let a=5;
+let b=3;
+let add=a+b;
+console.log("the addition of a and b" , add);
+let subtract=a-b;
+console.log("the subtraction of a and b" , subtract);
+let exp=a*b;
+console.log("a*b", exp)
+
+let x=9
+let number= x%2 ==0 ?
+"Even Number" : "Odd Number" ;
+console.log(number);
+
+let y=8
+number= y%2 ==0 ?
+"Even Number" : "Odd Number" ;
+console.log(number);
+
+
+let z=20;
+let c=z> 15?
+"Greater" : "Smaller" ;
+console.log(c);
+
+let age=19;
+let s=age> 18?
+"Can Vote" : "Can't Vote" ;
+console.log(s);
+
